7fb85f6cbb203a6ace9881af4d729d1e7e77a27e
Tony Nguyen <tng2001811@gmail.com>
2020-05-08 13:43:08 -0400

    added more servers for summoner search

--- src/app/components/summoner-search/summoner-search.component.ts
+++ src/app/components/summoner-search/summoner-search.component.ts
@@ -10,8 +10,15 @@ export class SummonerSearchComponent implements OnInit {
 
   servers: Server[] = [
     {title: 'North America', value: 'NA1'},
-    {title: 'Europe', value: 'EUN1'},
-    {title: 'Korea', value: 'KR'}
+    {title: 'Europe West', value: 'EUW1'},
+    {title: 'Korea', value: 'KR'},
+    {title: 'Europe Nordic & East', value: 'EUN1'},
+    {title: 'Japan', value: 'JP1'},
+    {title: 'Russia', value: 'RU'},
+    {title: 'Brazil', value: 'BR1'},
+    {title: 'Latin America', value: 'LA1'},
+    {title: 'Turkey', value: 'TR1'},
+    {title: 'Oceania', value: 'OC1'},
   ];
   
   summonerName: string = '';
